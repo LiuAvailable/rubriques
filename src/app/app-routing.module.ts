48f767f16b11b8e50f9e038bca47d369f1615ee6
Liu Santana <lsantana8@boscdelacoma.cat>
2022-12-20 19:28:14 +0100

    enrutament navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriteriComponent } from './projecte/components/criteri/criteri.component';
+import { ValoracioComponent } from './projecte/components/valoracio/valoracio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'valoracio', component: ValoracioComponent },
+  { path: 'criteri', component: CriteriComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
